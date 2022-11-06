8494f8e240775cbb31c1bdbd260ee06835701e6a
Sergii Polushkin <seregapola@gmail.com>
2022-11-06 01:17:22 +0100

    feat: finish third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const rootFolder = path.join(__dirname, "secret-folder");
+
+const fileBasename = (pathFile, ext) => {
+    return path.basename(pathFile, ext)
+};
+
+const fileExtension = (pathFile) => {
+    const ext = path.extname(pathFile);
+    return ext.slice(1);
+}
+
+fs.readdir(rootFolder, (err, data) => {
+    if(err) console.error(err.message);
+    data.forEach(item => {
+        const pathFile = path.join(rootFolder, `${item}`);
+        fs.stat(pathFile, (error, stats) => {
+            if(error){
+                console.log(error);
+            }else{
+                if(stats.isFile()){
+                    const ext = path.extname(pathFile);
+                    const basename = fileBasename(pathFile, ext);
+                    const extension = fileExtension(pathFile);
+                    const size = stats.size;
+                    const finishData = `${basename} - ${extension} - ${size}b`;
+                    console.log(finishData);
+             
+                }
+            }
+        });
+    });
+});
